133322cc708e2eb8102686a40e5ce5b3a39b24fb
satyendra <skchandrawansi03@gmail.com>
2024-10-17 19:22:45 +0530

    changed service model

--- models/service-model.js
+++ models/service-model.js
@@ -4,7 +4,8 @@ const serviceSchema = new mongoose.Schema({
   thumbnail: { type: String, require: true },
   name: { type: String, require: true },
   url: { type: String, require: true },
-  imageId: { type: String, require: true }
+  imageId: { type: String, require: true },
+  isAdmin: { type: Boolean, require: true }
 })
 
 const Service = mongoose.models.Service || new mongoose.model("Service", serviceSchema);
